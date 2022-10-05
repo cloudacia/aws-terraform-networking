c18425fd7dd5f6386aa3929cf783753fe609c582
Eugenio Duarte <eduarte@Eugenios-MacBook-Pro.local>
2022-10-05 17:16:35 -0400

    init commit

---
+++ single-ec2-vpc-endpoint-s3/ec2.tf
@@ -0,0 +1,29 @@
+#################################################
+# EC2 INSTANCE                                  #
+#################################################
+resource "aws_instance" "web01" {
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+  }
+  ami                         = var.aws_ami
+  instance_type               = var.ec2_type
+  subnet_id                   = aws_subnet.subnet01.id
+  user_data                   = filebase64("scripts/bootstrap.sh")
+  iam_instance_profile        = aws_iam_instance_profile.ec2-ssm-profile.name
+  associate_public_ip_address = true
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "optional"
+  }
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = true
+  }
+
+  tags = {
+    Name = "Development"
+  }
+}
